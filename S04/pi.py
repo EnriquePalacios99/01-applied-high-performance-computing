fea90952146a0d2d11be0ce3c3574b8983736c51
Enrique Palacios <97924191+EnriquePalacios99@users.noreply.github.com>
2025-04-26 16:38:51 +0000

    Añadir al Main

--- S04/pi.py
+++ S04/pi.py
@@ -1,4 +1,3 @@
-
 from mpi4py import MPI
 import time
 import math
@@ -7,12 +6,12 @@ t0 = time.time()
 
 # 1. Inicialice el universo de procesos y parametros 
 #    rank (id del proceso) y nproc (numero de procesos)
-#    . . .
-
-
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+nprocs = comm.Get_size()
 
 # 2. cantidad de intervalos de integracion
-nsteps = 16232234234 # use cantidades variables desde 2^10 a 2^24
+nsteps = 1024 # use cantidades variables desde 2^10 a 2^24
 # ancho de cada intervalo (eje x)
 dx = 1.0 / nsteps
 
@@ -33,6 +32,7 @@ else:
 
 # 3. se distribuyen los datos entre procesos (scatter)
 #    utilice comm.scatter(...)
+data = comm.scatter(data, root=0)
 
 # cada proceso realiza un calculo parcial (integracion)
 partial_pi = 0.0
@@ -43,8 +43,10 @@ partial_pi *= dx
 
 # 4. se recolectan resultados parciales 
 #    use comm.gather(...)
+partial_pi = comm.gather(partial_pi,root=0)
 
 # calculo del resultado final en el maestro
 if rank == 0:
     print('pi computed in {:.3f} sec'.format(time.time() - t0))
     print('error is {}'.format(abs(sum(partial_pi) - math.pi)))
+
